7a0779a3a22afbc4a90915b76312740481f399bd
Taha <you@example.com>
2021-01-12 02:40:48 +0300

    heroku finished

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance=axios.create({
-    baseURL:'https://tinder-back-heroku.herokuapp.com/'
+    baseURL:'https://tinder-back-heroku.herokuapp.com'
 })
 
 export default instance
